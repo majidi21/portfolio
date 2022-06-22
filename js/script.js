44041150a8643497ec61929f6eaf70422acdd4e7
majidi21 <62419385+majidi21@users.noreply.github.com>
2022-06-22 19:05:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -36,3 +36,10 @@ $(document).ready(function(){
   });
 
 });
+// typing
+var typed = new Typed(".typing", {
+  strings:["","Data Analist","Web Designer","Web Developer","Graphic Designer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+}) 
