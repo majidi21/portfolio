bad85806b4a1cb78e396d3b692abfefe3e181418
majidi21 <62419385+majidi21@users.noreply.github.com>
2022-06-30 18:47:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Majidi Salimu Mussa</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/skin/color-1.css">
+    <link rel="stylesheet" href="css/style-switcher.css">
+    <link rel="stylesheet" href="css/skin/color-1.css" class="alternative-style" title="color-1" disabled>
+    <link rel="stylesheet" href="css/skin/color-2.css" class="alternative-style" title="color-2" disabled>
+    <link rel="stylesheet" href="css/skin/color-3.css" class="alternative-style" title="color-3" disabled>
+    <link rel="stylesheet" href="css/skin/color-4.css" class="alternative-style" title="color-4" disabled>
+    <link rel="stylesheet" href="css/skin/color-5.css" class="alternative-style" title="color-5" disabled>
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <div class="user">
+        <img src="images/pic.jpg" alt="">
+        <h3 class="name">Majidi Salimu Mussa</h3>
+        <p class="post">Website developer</p>
+    </div>
+
+    <nav class="navbar">
+        <ul>
+            <li><a href="#home">home</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#education">education</a></li>
+            <li><a href="#portfolio">portfolio</a></li>
+            <li><a href="#contact">contact</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<!-- header section ends -->
+
+<div id="menu" class="fas fa-bars"></div>
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <h3>Hello, My name is <span class="name">Majidi Salimu Mussa</span></h3>
+    <h1>I'm a  <span class="typing">web Designer</span></h1>
+    <p>With professional experience in Data Analysis, Business Intelligence and 
+        Website Development like E-commerce, Marketing or Business Website, Blog or Media Outlet, 
+        Education Website and Portfolio or Personal website. Also, am enthusiastic, obedient, trustworthy 
+        and hardworking person able to work in any conditions. Open to connect and share my skills and 
+        experiences
+    </p>
+    <a href="#about"><button class="btn">about me <i class="fas fa-user"></i></button></a>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading"> <span>about</span> me </h1>
+
+<div class="row">
+
+    <div class="info">
+        <h3> <span> name : </span> Majidi Salimu Mussa </h3>
+        <h3> <span> qualification : </span> Bsc ANEB </h3>
+        <h3> <span> post : </span> Website developer </h3>
+        <h3> <span> language : </span> English </h3>
+        <h3> <span> language : </span> Swahili </h3>
+        <a href="cv Majidi Salimu.pdf"><button class="btn"> download CV <i class="fas fa-download"></i> </button></a>
+    </div>
+<!--
+   /* <div class="counter">
+        <div class="box">
+            <span>2+</span>
+            <h3>years of experience</h3>
+        </div>
+        <div class="box">
+            <span>100+</span>
+            <h3>porject completed</h3>
+        </div>
+        <div class="box">
+            <span>430+</span>
+            <h3>happy clients</h3>
+        </div>
+        <div class="box">
+            <span>12+</span>
+            <h3>awards won</h3>
+        </div>
+    </div> 
+-->
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- education section starts  -->
+
+<section class="education" id="education">
+
+<h1 class="heading"> my <span>education</span> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>JANUARY 2012- DEC 2015 </span>
+        <h3>SHITA SECONDARY SCHOOL</h3>
+        <p>Certificate for secondary education</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>JULY 2016- MAY 2018</span>
+        <h3>GALANOS SECOCONDARY SCHOOL</h3>
+        <p>Advanced certificate for secondary education <br>
+            Course: Economics, Geography and History
+            </p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>NOV 2018 - JUL 2021</span>
+        <h3>UNIVERSITY OF DAR ES SALAAM</h3>
+        <p>Course: BSC in Agricultural and Natural Resource Economics and Business</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2015</span>
+        <h3>Data Analysis Online Studies</h3>
+        <p>Microsoft power bi, Microsoft SQL, Table</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2016</span>
+        <h3>Website development Online Studies</h3>
+        <p>R-script, HTML, 
+            CSS</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-graduation-cap"></i>
+        <span>2017</span>
+        <h3>Website development Online Studies</h3>
+        <p>PHP, JavaScript, jQuery, Bootstrap and SQL script</p>
+    </div>
+
+</div>
+
+</section>
+
+<!-- education section ends -->
+
+<!-- portfolio section starts  -->
+
+<section class="portfolio" id="portfolio">
+
+<h1 class="heading"> my <span>portfolio</span> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <img src="images/img1.jpg" alt="">
+    </div>
+
+    <div class="box">
+        <img src="images/img2.jpg" alt="">
+    </div>
+
+    <div class="box">
+        <img src="images/img3.jpg" alt="">
+    </div>
+
+    <div class="box">
+        <img src="images/img4.jpg" alt="">
+    </div>
+
+    <div class="box">
+        <img src="images/img5.jpg" alt="">
+    </div>
+
+    <div class="box">
+        <img src="images/img6.jpg" alt="">
+    </div>
+
+</div>
+
+</section>
+
+<!-- portfolio section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+<h1 class="heading"> <span>contact</span> me </h1>
+
+<div class="row">
+
+    <div class="content">
+
+        <h3 class="title">contact info</h3>
+
+        <div class="info">
+            <h3> <i class="fas fa-envelope"></i> mjdmakumba@gmail.com </h3>
+            <h3> <i class="fas fa-phone"></i> +255 789466546 </h3>
+            <h3> <i class="fas fa-phone"></i> +255 753872261 </h3>
+            <h3> <i class="fas fa-map-marker-alt"></i> Tanga, Tanzania - 21620. </h3>
+        </div>
+
+    </div>
+
+    <form id="myForm">
+
+    <h4 class="sent-notification" style=" font-size: 2rem;color:var(--text-black-900);"></h4>
+        <input type="text" id="name" placeholder="name" class="box">
+        <input type="email" id="email" placeholder="email" class="box">
+        <input type="text" id="subject" placeholder="Subject" class="box">
+        <textarea name="" id="body" cols="30" rows="10" class="box message" placeholder="message"></textarea>
+        <button type="button" class="btn" onclick="sendEmail()" value="Send An Email"> send <i class="fas fa-paper-plane"></i> </button>
+
+    </form>
+
+</div>
+
+</section>
+
+<!-- contact section ends -->
+
+
+<!-- scroll top button  -->
+
+<a href="#home" class="top">
+    <img src="images/scroll-top-img.png" alt="">
+</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jquery cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js" referrerpolicy="no-referrer"></script>
+<!-- style switcher start -->
+<div class="style-switcher">
+    <div class="style-switcher-toggler s-icon">
+        <i class="fas fa-cog fa-spin"></i>
+    </div>
+    <div class="day-night s-icon">
+        <i class="fas "></i>
+    </div>
+    <h4>Theme Colors</h4>
+    <div class="colors">
+        <span class="color-1" onclick="setActiveStyle('color-1')"></span>
+        <span class="color-2" onclick="setActiveStyle('color-2')"></span>
+        <span class="color-3" onclick="setActiveStyle('color-3')"></span>
+        <span class="color-4" onclick="setActiveStyle('color-4')"></span>
+        <span class="color-5" onclick="setActiveStyle('color-5')"></span>
+    </div>
+</div>
+<!-- style switcher end -->
+<script src="js/style-switcher.js"></script>
+<script src="js/typing.js"></script>
+<!-- custom js file link  -->
+<!-- Contact Us Start  -->
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript">
+        function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var subject = $("#subject");
+            var body = $("#body");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       subject: subject.val(),
+                       body: body.val()
+                   }, success: function (response) {
+                        $('#myForm')[0].reset();
+                        $('.sent-notification').text("Message Sent Successfully.");
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+    </script>
+</body>
+</html>
