1e39520a702522e2ac17a43477c3b6e237ff0ff3
majidi21 <62419385+majidi21@users.noreply.github.com>
2022-06-30 18:48:59 +0300

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,7 @@
+// typing
+var typed = new Typed(".typing", {
+    strings:["","Data Analist","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+  }) 
